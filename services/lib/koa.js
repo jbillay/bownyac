5cfab59a501add769780a3f6f3dcf36cb3a6f63a
Jeremy Billay <jbilla@gmail.com>
2018-03-11 18:42:04 +0000

    Add API documentation structure

--- services/lib/koa.js
+++ services/lib/koa.js
@@ -7,6 +7,21 @@ const create = async function create(config) {
   app.context.db = await dbConnect.connect(config.db.server, config.db.dbName);
 
   // TODO: Remove that test
+  /**
+   * @api {get} / Test function
+   * @apiVersion 0.0.1
+   * @apiName Test
+   * @apiGroup Test
+   *
+   * @apiSuccess {String} data Confirmation message
+   *
+   * @apiSuccessExample {jsonp} Success-Response:
+   *     HTTP/1.1 200 OK
+   *     {
+   *        "data": "Hello World"
+   *     }
+   *
+   */
   app.use(KoaRouter.get('/', async (ctx) => {
     ctx.body = {
       data: 'Hello World',
